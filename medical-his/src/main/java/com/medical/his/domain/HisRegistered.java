09c41fa9193a46d03003ddb648b813c08032d9fe
chenshiyang <c18570603433@163.com>
2022-12-05 21:37:07 +0800

    检查费用设置模块

---
+++ medical-his/src/main/java/com/medical/his/domain/HisRegistered.java
@@ -0,0 +1,22 @@
+package com.medical.his.domain;
+
+import com.medical.common.core.domain.BaseEntity;
+import lombok.Data;
+
+/**
+ * @author Csy
+ * @date 2022/12/5 21:30
+ */
+@Data
+public class HisRegistered extends BaseEntity {
+    private static final long serialVersionUID = 1L;
+
+    /**挂号费用ID*/
+    private Long registeredId;
+
+    /**挂号费名称*/
+    private String registeredName;
+
+    /**挂号费*/
+    private String registeredCost;
+}
